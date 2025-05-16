796740e52deedb8b7bccc1bf9914193f1deb20f9
Kavindalgl <lakshithakavinda7@gmail.com>
2025-05-16 15:54:57 +0530

    mqtt Client implemented

--- mqttClient.cpp
+++ mqttClient.cpp
@@ -5,18 +5,30 @@
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
 
+void setupWifi();
+void setupMqtt();
+void connectToBroker();
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   setupWifi();
+  setupMqtt();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(mqttClient.connected()) {
+  if(!mqttClient.connected()) {
     connectToBroker();
   }
+
+//to check the available msgs
+  mqttClient.loop();
+
+//pusblishing temperature values to the topic
+  mqttClient.publish("ENTC-TEMP", "25.24");
+  delay(1000);
 
 }
 
@@ -37,15 +49,19 @@ void setupWifi() {
 
 void setupMqtt() {
   //setting up mqtt server
-  mqttClient.setServer("test.mosquitto.org", 1883);
+  mqttClient.setServer("broker.hivemq.com", 1883);
 }
 
 void connectToBroker(){
   while (!mqttClient.connected()){
-    Serial.print("Attempting MQTT connection");
-    mqttClient.connect();
-    connectToBroker();
+    Serial.print("Attempting MQTT connection ");
+    if(mqttClient.connect("ESP32")){
+      Serial.println("connected");
+    }else{
+      Serial.print("failed");
+      Serial.print(mqttClient.state());
+      delay(5000);
+    }
   }
-
 }
 
